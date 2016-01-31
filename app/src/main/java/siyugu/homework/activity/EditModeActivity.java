e041e4e63557b6252a597de7e123463238c616cc
Siyu Gu <siyugu@siyugu-macbookair.roam.corp.google.com>
2016-01-31 11:43:54 -0800

    minor tune editmode

--- app/src/main/java/siyugu/homework/activity/EditModeActivity.java
+++ app/src/main/java/siyugu/homework/activity/EditModeActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -61,6 +62,8 @@ public class EditModeActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_mode);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
     mCurrentPhotoPath = null;
 

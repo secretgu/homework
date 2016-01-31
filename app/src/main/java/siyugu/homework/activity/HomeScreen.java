ecf2494c575178be41ed572f91e5ea826d3e7749
Siyu Gu <siyugu@siyugu-macbookair.roam.corp.google.com>
2016-01-31 11:01:49 -0800

    TabView

---
+++ app/src/main/java/siyugu/homework/activity/HomeScreen.java
@@ -0,0 +1,35 @@
+package siyugu.homework.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import siyugu.homework.R;
+import siyugu.homework.fragment.CalendarFragment;
+import siyugu.homework.fragment.ReferenceFragment;
+import siyugu.homework.fragment.SimplePagerAdapter;
+import siyugu.homework.fragment.TodayFragment;
+
+
+public class HomeScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home_screen);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+    final SimplePagerAdapter adapter = new SimplePagerAdapter(getSupportFragmentManager());
+    adapter.addFragment(new TodayFragment(), "Today");
+    adapter.addFragment(new CalendarFragment(), "Calendar");
+    adapter.addFragment(new ReferenceFragment(), "Reference");
+    viewPager.setAdapter(adapter);
+
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+    tabLayout.setupWithViewPager(viewPager);
+  }
+}
